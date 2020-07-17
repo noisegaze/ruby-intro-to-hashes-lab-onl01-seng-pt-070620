d22d0b4714d6f106453ce371bc32dc64a8aafdc7
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-17 21:04:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_fourth_tier
 	 monopoly = {
 	  :railroads =>{:pieces => 4,
 	    :names => {},
-	    :rent_in_dollars => {:one_piece_owned => 25}
+	    :rent_in_dollars => {:one_piece_owned => 25,:two_pieces_owned => 50}
 	  }
 	}
 
